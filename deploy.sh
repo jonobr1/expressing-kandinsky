c9fe67403910be740c446fd6c78724053c54503d
Jono <developer@jono.fyi>
2019-11-15 13:52:46 +0100

    Improving Distribution and Deployment

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd site/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'www.expressing-kandinsky.net' > CNAME
+echo 'expressing-kandinsky.net' > CNAME
 
 git init
 git add -A
